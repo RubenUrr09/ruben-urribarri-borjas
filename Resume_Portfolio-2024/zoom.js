7f8020b94f78734662655c00d8877d748b72a356
Ruben Urribarri <159847550+RubenUrr09@users.noreply.github.com>
2025-04-17 03:59:41 -0500

    Add files via upload

---
+++ Resume_Portfolio-2024/zoom.js
@@ -0,0 +1,7 @@
+// Detect mobile devices
+  if (window.innerWidth <= 768) {
+    const viewport = document.querySelector("meta[name=viewport]");
+    if (viewport) {
+      viewport.setAttribute("content", "width=device-width, initial-scale=0.8");
+    }
+  }
